d264eead03e183d0f314acf36e8dd044e6c2c10e
cezary.rejczyk <cezary.rejczyk@capsilon.com>
2018-07-04 16:26:26 +0200

    [Fix] Code refactoring

--- src/main/java/com/softmill/reactivespringboot/reactivespringboot/repository/ChapterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softmill.reactivespringboot.reactivespringboot.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import com.softmill.reactivespringboot.reactivespringboot.model.Chapter;
-
-public interface ChapterRepository extends ReactiveCrudRepository<Chapter, String> {
-
-}
